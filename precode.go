4ed66da68ebb17b5e0a0ac88acaf73d2cd62138d
Johan Kvint <johannkvint@yandex.ru>
2024-06-05 20:07:43 +0300

    second iteration

--- precode.go
+++ precode.go
@@ -40,19 +40,17 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// getAllTasks возвращает все задачи из карты tasks.
-func getAllTasks(w http.ResponseWriter, r *http.Request) {
+func getTasks(w http.ResponseWriter, r *http.Request) { // Изменено название функции
 	tasksList := make([]Task, 0, len(tasks))
 	for _, task := range tasks {
 		tasksList = append(tasksList, task)
 	}
 	json.NewEncoder(w).Encode(tasksList)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
 
-// sendTask принимает задачу в теле запроса и сохраняет ее в карте tasks.
-func sendTask(w http.ResponseWriter, r *http.Request) {
+func addTask(w http.ResponseWriter, r *http.Request) {
 	var newTask Task
 	err := json.NewDecoder(r.Body).Decode(&newTask)
 	if err != nil {
@@ -64,39 +62,36 @@ func sendTask(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
-// getTaskByID возвращает задачу с указанным ID, если она существует в карте tasks.
-func getTaskByID(w http.ResponseWriter, r *http.Request) {
+func getTask(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	task, ok := tasks[id]
 	if !ok {
-		http.NotFound(w, r)
+		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
 	json.NewEncoder(w).Encode(task)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
 
-// deleteTaskByID удаляет задачу из карты tasks по ее ID, если она существует.
-func deleteTaskByID(w http.ResponseWriter, r *http.Request) {
+func deleteTask(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	_, ok := tasks[id]
 	if !ok {
-		http.NotFound(w, r)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
 	delete(tasks, id)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	r := chi.NewRouter()
-
-	// здесь регистрируйте ваши обработчики
-	r.Get("/tasks", getAllTasks)
-	r.Post("/tasks", sendTask)
-	r.Get("/tasks/{id}", getTaskByID)
-	r.Delete("/tasks/{id}", deleteTaskByID)
-
+	r.Get("/tasks", getTasks) // Изменено название функции
+	r.Post("/tasks", addTask)
+	r.Get("/tasks/{id}", getTask)
+	r.Delete("/tasks/{id}", deleteTask)
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
