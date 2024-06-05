fc6fa1000c90f1654fac85793df5b398e3c5567f
Johan Kvint <johannkvint@yandex.ru>
2024-06-05 17:25:25 +0300

    first commit

--- precode.go
+++ precode.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -40,13 +41,61 @@ var tasks = map[string]Task{
 }
 
 // Ниже напишите обработчики для каждого эндпоинта
-// ...
+// getAllTasks возвращает все задачи из карты tasks.
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+	tasksList := make([]Task, 0, len(tasks))
+	for _, task := range tasks {
+		tasksList = append(tasksList, task)
+	}
+	json.NewEncoder(w).Encode(tasksList)
+	w.WriteHeader(http.StatusOK)
+}
+
+// sendTask принимает задачу в теле запроса и сохраняет ее в карте tasks.
+func sendTask(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	tasks[newTask.ID] = newTask
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+// getTaskByID возвращает задачу с указанным ID, если она существует в карте tasks.
+func getTaskByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	task, ok := tasks[id]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	json.NewEncoder(w).Encode(task)
+	w.WriteHeader(http.StatusOK)
+}
+
+// deleteTaskByID удаляет задачу из карты tasks по ее ID, если она существует.
+func deleteTaskByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	_, ok := tasks[id]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	delete(tasks, id)
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAllTasks)
+	r.Post("/tasks", sendTask)
+	r.Get("/tasks/{id}", getTaskByID)
+	r.Delete("/tasks/{id}", deleteTaskByID)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
